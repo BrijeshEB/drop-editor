483e5b93eafcb8fbf55f05bbc29f53ba7bcfeff5
BrijeshEB <brijesh.b@engineerbabu.in>
2021-06-24 11:54:25 +0530

    First commit

---
+++ src/components/TextEditor.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
+import { List } from 'semantic-ui-react'
+import { Draggable, Droppable } from 'react-drag-and-drop'
+
+export default class TextEditor extends Component {
+
+  onDrop(data) {
+    console.log(data)
+    // => banana 
+  }
+
+  state = {
+    editorState: EditorState.createEmpty(),
+  };
+
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+    return (
+      <div>
+            <ul>
+                <Draggable type="fruit" data="banana"><li>Banana</li></Draggable>
+                <Draggable type="fruit" data="apple"><li>Apple</li></Draggable>
+                <Draggable type="metal" data="silver"><li>Silver</li></Draggable>
+            </ul>
+            <Droppable
+                types={['fruit']} // <= allowed drop types
+                onDrop={this.onDrop.bind(this)}>
+                <ul className="Smoothie"></ul>
+            </Droppable>
+        <br/>
+        <div className="userNameList">
+          <List divided>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+          </List>
+        </div>
+        <Editor
+          style={{border:'solid 1px #000000'}}
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        <textarea
+          style={{width:'100%',height:'200px'}}
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        ></textarea>
+      </div>
+    );
+  }
+}
